023d40738cfe48b174e1299a31d6d1680f1ec41c
Ivy <69495341+AtenB3f@users.noreply.github.com>
2023-02-17 16:27:30 +0900

    Initial Commit

---
+++ ImageDownload/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageDownload
+//
+//  Created by Ivy Moon on 2023/02/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
